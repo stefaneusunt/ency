1813610eeb2643fac8f83b18cb39852f8779365d
Maximovici Stefan <stefaneusunt@gmail.com>
2017-04-25 16:15:48 +0300

    Am creat componentele

---
+++ src/app/document-preview/document-preview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ency-document-preview',
+  templateUrl: './document-preview.component.html',
+  styleUrls: ['./document-preview.component.scss']
+})
+export class DocumentPreviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
